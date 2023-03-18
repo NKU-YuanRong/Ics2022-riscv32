9319a80273a739db288a9844e55a9a25eeced820
tracer-ics2022 <tracer@njuics.org>
2023-03-18 19:54:47 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  19:54:47 up 55 min,  1 user,  load average: 0.34, 0.38, 0.40

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -118,6 +118,7 @@ void cpu_exec(uint64_t n) {
            (nemu_state.halt_ret == 0 ? ANSI_FMT("HIT GOOD TRAP", ANSI_FG_GREEN) :
             ANSI_FMT("HIT BAD TRAP", ANSI_FG_RED))),
           nemu_state.halt_pc);
+      Log("Haihaihai");
       // fall through
     case NEMU_QUIT: statistic();
   }
