6553ade9d6fb3613a358b7fe9be98761cc6e9ed3
tracer-ics2022 <tracer@njuics.org>
2023-03-19 22:45:01 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:45:01 up  8:10,  1 user,  load average: 1.00, 0.80, 0.93

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -113,7 +113,7 @@ static bool make_token(char *e) {
          * to record the token in the array `tokens'. For certain types
          * of tokens, some extra actions should be performed.
          */
-        printf("Match this: %d", rules[i].token_type);
+        
         switch (rules[i].token_type) {
           case TK_NOTYPE:
             break;
