6796623c411c3b284af35009c6569f1bbe22f5a8
tracer-ics2022 <tracer@njuics.org>
2023-03-19 23:17:49 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:17:49 up  8:43,  1 user,  load average: 1.04, 1.14, 1.02

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -186,7 +186,7 @@ word_t expr(char *e, bool *success) {
   Log("match succesfully! valid token number: %d\n", nr_token);
 
   for (int i = 0; i < nr_token; i++) {
-    printf("Token%d-Type:-%d,-Value:-%s", i, tokens[i].type, tokens[i].str);
+    printf("Token%d-Type:-%d,-Value:-%s\n", i, tokens[i].type, tokens[i].str);
   }
 
   return 1;
