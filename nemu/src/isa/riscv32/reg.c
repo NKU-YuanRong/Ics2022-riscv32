dd0b8103ec327fbb94c39c1ffad3ba2120e2c35d
tracer-ics2022 <tracer@njuics.org>
2023-03-19 13:24:22 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:24:22 up  4:01,  1 user,  load average: 2.81, 2.19, 1.59

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -24,7 +24,11 @@ const char *regs[] = {
 };
 
 void isa_reg_display() {
-  printf("pc\t%04x", cpu.pc);
+  printf("pc\t%04x\n", cpu.pc);
+  int i;
+  for (i = 0; i < ARRLEN(regs); i++) {
+    printf("%s\t%04x\n", regs[i], cpu.gpr[i]);
+  }
 }
 
 word_t isa_reg_str2val(const char *s, bool *success) {
