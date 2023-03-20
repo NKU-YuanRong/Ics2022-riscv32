6d58020048ce55a2630fe2098afcb257a0fca767
tracer-ics2022 <tracer@njuics.org>
2023-03-20 21:52:51 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:52:51 up 18:49,  1 user,  load average: 1.26, 1.00, 0.99

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -28,6 +28,9 @@ typedef struct watchpoint {
 static WP wp_pool[NR_WP] = {};
 static WP *head = NULL, *free_ = NULL;
 
+WP* new_wp();
+void free_wp(WP *wp);
+
 void init_wp_pool() {
   int i;
   for (i = 0; i < NR_WP; i ++) {
@@ -41,16 +44,68 @@ void init_wp_pool() {
 
 /* TODO: Implement the functionality of watchpoint */
 
+bool Insert_to_head(WP *np) {
+  if (!head) {
+    head = np;
+    np->next = NULL;
+    return true;
+  }
+  WP* p = head;
+  head = np;
+  np->next = p;
+  return true;
+}
+
+bool Insert_to_free_(WP *np) {
+  if (!free_) {
+    free_ = np;
+    np->next = NULL;
+    return true;
+  }
+  WP* p = free_;
+  free_ = np;
+  np->next = p;
+  return true;
+}
+
 // get a new free watch point
 WP* new_wp() {
-  if (!head) {
+  if (!free_) {
     Log(ANSI_FMT("No free watch point!", ANSI_FG_RED));
     return NULL;
   }
-  return NULL;
+  // Find first free node
+  WP *p = free_;
+  free_ = p->next;
+  Insert_to_head(p);
+  return p;
 }
 
 // free the watch point
 void free_wp(WP *wp) {
-
+  if (!head) {
+    Log(ANSI_FMT("No head watch point!", ANSI_FG_RED));
+    return;
+  }
+  if (head == wp) {
+    head = head->next;
+    Insert_to_free_(wp);
+    Log("Free watch point %d", wp->NO);
+    return;
+  }
+  // Find first free node
+  WP *p = head;
+  WP *q = p->next;
+  while (q) {
+    if (q == wp) {
+      p->next = q->next;
+      Insert_to_free_(q);
+      Log("Free watch point %d", q->NO);
+      return;
+    }
+    p = q;
+    q = q->next;
+  }
+  Log(ANSI_FMT("The watch point is free, no need to free it again!", ANSI_FG_RED));
+  return;
 }
