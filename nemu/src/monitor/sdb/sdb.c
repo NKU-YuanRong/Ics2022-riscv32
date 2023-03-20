0ad22fadb61fcde43c647b8d339e16bada57db27
tracer-ics2022 <tracer@njuics.org>
2023-03-20 21:03:05 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:03:05 up 17:59,  1 user,  load average: 1.04, 0.99, 1.02

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -308,7 +308,8 @@ void gen_rand_expr(char *exp) {
     return;
   }
   char num[10];
-  uint32_t switch_num = (2 + exp_len < 5 ? 2 + exp_len: 5);
+  // uint32_t switch_num = (2 + exp_len < 5 ? 2 + exp_len: 5);
+  uint32_t switch_num = 5;
   switch (rand() % switch_num) {
     case 0:
     case 1:
