6ef632293a7faed00145b68170f81f2e8226b5ec
tracer-ics2022 <tracer@njuics.org>
2023-05-18 16:48:17 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:48:17 up 1 day,  7:42,  1 user,  load average: 1.20, 0.93, 0.80

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,7 +5,7 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  uptime->us = ((uint64_t)inl(RTC_ADDR+4) << 32) + (uint64_t)inl(RTC_ADDR);
+  uptime->us = ((uint64_t)inl(RTC_ADDR + 4) << 32) | (uint64_t)inl(RTC_ADDR);
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
@@ -14,5 +14,5 @@ void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
   rtc->hour   = 0;
   rtc->day    = 0;
   rtc->month  = 0;
-  rtc->year   = 1900;
+  rtc->year   = 2023;
 }
