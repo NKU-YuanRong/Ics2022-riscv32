2a6e1d0c3c5cedfc6d06a779205e0016494c7140
tracer-ics2022 <tracer@njuics.org>
2023-05-18 16:44:02 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:44:02 up 1 day,  7:38,  1 user,  load average: 2.03, 0.87, 0.74

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -1,3 +1,121 @@
+// #include <am.h>
+// #include <klib.h>
+// #include <klib-macros.h>
+// #include <stdarg.h>
+
+// #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
+
+// static char* get_int(char *p, va_list *ap) {
+// 	int d = va_arg(*ap, int);
+// 	char str[32];
+// 	int len = 0;
+// 	if (d == 0) {
+// 		*p++ = '0';
+// 		return p;
+// 	}
+// 	if (d < 0) {
+// 		*p++ = '-';
+// 		d *= -1;
+// 	}
+// 	while (d) {
+// 		str[len++] = d % 10 + '0';
+// 		d /= 10;
+// 	}
+// 	for (int i = len-1; i >= 0; i--) {
+// 		*p++ = str[i];
+// 	}
+// 	return p;
+// }
+
+// static char* get_string(char* p, va_list *ap) {
+// 	char *str = va_arg(*ap, char*);
+// 	while(*str) {
+// 		*p++ = *str++;
+// 	}
+// 	return p;
+// }
+
+// static char* get_char(char* p, va_list *ap) {
+// 	char ch = (char)va_arg(*ap, int);
+// 	*p++ = ch;
+// 	return p;
+// }
+
+// int printf(const char *fmt, ...) {
+// 	char out[1024];
+// 	va_list ap;
+// 	va_start(ap, fmt);
+// 	char* p = (char*)out;
+// 	while (*fmt) {
+// 		if (*fmt == '%') {
+// 			fmt++;
+// 			switch (*fmt) {
+// 				case 'd': 
+// 					p = get_int(p, &ap);
+// 					break;
+// 				case 's': 
+// 					p = get_string(p, &ap);
+// 					break;
+// 				case 'c':
+// 					p = get_char(p, &ap);
+// 					break;
+// 			}
+// 			fmt++;
+// 		}
+// 		else {
+// 			*p++ = *fmt++;
+// 		}
+// 	}
+// 	*p++ = '\0';
+// 	va_end(ap);
+// 	putstr(out);
+// 	return 0;
+// }
+
+// int vsprintf(char *out, const char *fmt, va_list ap) {
+//   panic("Not implemented");
+// }
+
+// int sprintf(char *out, const char *fmt, ...) {
+// 	va_list ap;
+// 	va_start(ap, fmt);
+//     char* p = (char*)out;
+// 	while (*fmt) {
+// 		if (*fmt == '%') {
+// 			fmt++;
+// 			switch (*fmt) {
+// 				case 'd': 
+//           p = get_int(p, &ap);
+//           break;
+// 				case 's': 
+//           p = get_string(p, &ap);
+//           break;
+// 				case 'c':
+//           p = get_char(p, &ap);
+//           break;
+// 			}
+// 			fmt++;
+// 		}
+// 		else {
+// 			*p++ = *fmt++;
+// 		}
+// 	}
+// 	*p++ = '\0';
+// 	va_end(ap);
+// 	return 0;
+//   //panic("Not implemented");
+// }
+
+// int snprintf(char *out, size_t n, const char *fmt, ...) {
+//   panic("Not implemented");
+// }
+
+// int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+//   panic("Not implemented");
+// }
+
+// #endif
+
 #include <am.h>
 #include <klib.h>
 #include <klib-macros.h>
@@ -41,24 +159,15 @@ static char* get_char(char* p, va_list *ap) {
 	return p;
 }
 
-int printf(const char *fmt, ...) {
-  char out[1024];
-	va_list ap;
-	va_start(ap, fmt);
-  char* p = (char*)out;
+static int make_out(char *out, const char *fmt, va_list ap) {
+	char* p = (char*)out;
 	while (*fmt) {
 		if (*fmt == '%') {
 			fmt++;
 			switch (*fmt) {
-				case 'd': 
-          p = get_int(p, &ap);
-          break;
-				case 's': 
-          p = get_string(p, &ap);
-          break;
-				case 'c':
-          p = get_char(p, &ap);
-          break;
+				case 'd': p = get_int(p, &ap);break;
+				case 's': p = get_string(p, &ap);break;
+				case 'c': p = get_char(p, &ap);break;
 			}
 			fmt++;
 		}
@@ -67,6 +176,14 @@ int printf(const char *fmt, ...) {
 		}
 	}
 	*p++ = '\0';
+	return 0;
+}
+
+int printf(const char *fmt, ...) {
+  char out[2048];
+	va_list ap;
+	va_start(ap, fmt);
+	make_out(out, fmt, ap);
 	va_end(ap);
 	putstr(out);
 	return 0;
@@ -79,28 +196,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 int sprintf(char *out, const char *fmt, ...) {
 	va_list ap;
 	va_start(ap, fmt);
-    char* p = (char*)out;
-	while (*fmt) {
-		if (*fmt == '%') {
-			fmt++;
-			switch (*fmt) {
-				case 'd': 
-          p = get_int(p, &ap);
-          break;
-				case 's': 
-          p = get_string(p, &ap);
-          break;
-				case 'c':
-          p = get_char(p, &ap);
-          break;
-			}
-			fmt++;
-		}
-		else {
-			*p++ = *fmt++;
-		}
-	}
-	*p++ = '\0';
+	make_out(out, fmt, ap);
 	va_end(ap);
 	return 0;
   //panic("Not implemented");
