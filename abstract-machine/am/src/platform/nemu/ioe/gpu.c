8b0d1d0831d195bc989876b5c25da6fd7953991b
tracer-ics2022 <tracer@njuics.org>
2023-05-23 15:05:21 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:05:21 up 1 day, 10:54,  1 user,  load average: 0.12, 0.23, 0.28

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -25,15 +25,31 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   };
 }
 
+// void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+// 	int i, j;
+// 	int screen_width = inl(VGACTL_ADDR) >> 16;
+// 	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+// 	uint32_t *pixels = ctl->pixels;
+// 	for (i = ctl->y; i < ctl->y + ctl->h; i++)
+// 		for (j = ctl->x; j < ctl->x + ctl->w; j++) {
+// 			// outl((uintptr_t)(fb+(ctl->x+j)+(ctl->y+i)*screen_width), pixels[i*ctl->w+j]);
+// 			fb[screen_width*i+j] = pixels[ctl->w*(i-ctl->y)+(j-ctl->x)];
+// 		}
+
+//   if (ctl->sync) {
+//     outl(SYNC_ADDR, 1);
+//   }
+
+// }
+
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 	int i, j;
-	int screen_width = inl(VGACTL_ADDR) >> 16;
+	int screen_width = io_read(AM_GPU_CONFIG).width;
 	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-	uint32_t *pixels = ctl->pixels;
-	for (i = ctl->y; i < ctl->y + ctl->h; i++)
-		for (j = ctl->x; j < ctl->x + ctl->w; j++) {
-			// outl((uintptr_t)(fb+(ctl->x+j)+(ctl->y+i)*screen_width), pixels[i*ctl->w+j]);
-			fb[screen_width*i+j] = pixels[ctl->w*(i-ctl->y)+(j-ctl->x)];
+	uint32_t *pixels = (uint32_t *)ctl->pixels;
+	for (i = 0; i < ctl->h; i++)
+		for (j = 0; j < ctl->w; j++) {
+			outl((uintptr_t)(fb+(ctl->x+j)+(ctl->y+i)*screen_width), pixels[i*ctl->w+j]);
 		}
 
   if (ctl->sync) {
