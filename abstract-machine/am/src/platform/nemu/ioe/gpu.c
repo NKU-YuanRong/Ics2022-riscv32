390f327559bef09e6b09e1968323ccbbfa66ab52
tracer-ics2022 <tracer@njuics.org>
2023-05-23 13:17:20 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:17:20 up 1 day,  9:06,  1 user,  load average: 0.89, 0.84, 0.83

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -1,44 +1,44 @@
 #include <am.h>
 #include <nemu.h>
-#include <stdio.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-	int i;
-	int temp = inl(VGACTL_ADDR);
-	int w = temp >> 16;
-	int h = temp & 0xFFFF;
-	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-	for (i = 0; i < w * h; i++) fb[i] = i;
-	outl(SYNC_ADDR, 1);
+  // int i;
+  // int w = 0;  // TODO: get the correct width
+  // int h = 0;  // TODO: get the correct height
+  // uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  // for (i = 0; i < w * h; i ++) fb[i] = i;
+  // outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
-	int temp = inl(VGACTL_ADDR);
-	int width = temp >> 16;
-	int height = temp & 0xFFFF;
+  uint32_t info = inl(VGACTL_ADDR);
+  uint16_t height = (uint16_t)(info & 0xFFFF);
+  uint16_t width = (uint16_t)(info >> 16);
   *cfg = (AM_GPU_CONFIG_T) {
-    .present = true, .has_accel = false,
-    .width = width, .height = height,
+    .present = true, 
+    .has_accel = false,
+    .width = width, 
+    .height = height,
     .vmemsz = 0
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
-	int i, j;
-	int screen_width = io_read(AM_GPU_CONFIG).width;
-	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-	uint32_t *pixels = (uint32_t *)ctl->pixels;
-	for (i = 0; i < ctl->h; i++)
-		for (j = 0; j < ctl->w; j++) {
-			outl((uintptr_t)(fb+(ctl->x+j)+(ctl->y+i)*screen_width), pixels[i*ctl->w+j]);
-		}
-
+  int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+  if (!ctl->sync && (w == 0 || h == 0)) return;
+  uint32_t *pixels = ctl->pixels;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t screen_w = inl(VGACTL_ADDR) >> 16;
+  for (int i = y; i < y+h; i++) {
+    for (int j = x; j < x+w; j++) {
+      fb[screen_w*i+j] = pixels[w*(i-y)+(j-x)];
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
-
 }
 
 void __am_gpu_status(AM_GPU_STATUS_T *status) {
