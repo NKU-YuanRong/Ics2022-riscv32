f60dac47f3ba36310f56698d229e9c2337db009d
tracer-ics2022 <tracer@njuics.org>
2023-05-23 12:59:02 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:59:02 up 1 day,  8:48,  1 user,  load average: 0.83, 0.81, 0.65

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -5,6 +5,6 @@
 
 void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   int scancode = inl(KBD_ADDR);
-	kbd->keydown = (scancode & KEYDOWN_MASK) ? true : false;
+	kbd->keydown = (scancode & KEYDOWN_MASK) ? 1 : 0;
   kbd->keycode = scancode & (~KEYDOWN_MASK);
 }
