ba0fabc8ca3c13c4085260d7a8c07dd4dc2e3a4f
tracer-ics2022 <tracer@njuics.org>
2023-05-23 14:56:47 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:56:47 up 1 day, 10:46,  1 user,  load average: 0.08, 0.15, 0.31

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -29,9 +29,9 @@ void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
 	int i, j;
 	int screen_width = inl(VGACTL_ADDR) >> 16;
 	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-	uint32_t *pixels = (uint32_t *)ctl->pixels;
-	for (i = ctl->x; i < ctl->x + ctl->h; i++)
-		for (j = ctl->y; j < ctl->y + ctl->w; j++) {
+	uint32_t *pixels = ctl->pixels;
+	for (i = ctl->y; i < ctl->y + ctl->h; i++)
+		for (j = ctl->x; j < ctl->x + ctl->w; j++) {
 			// outl((uintptr_t)(fb+(ctl->x+j)+(ctl->y+i)*screen_width), pixels[i*ctl->w+j]);
 			fb[screen_width*i+j] = pixels[ctl->w*(i-ctl->y)+(j-ctl->x)];
 		}
