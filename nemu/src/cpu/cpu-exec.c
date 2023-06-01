7f4fc7a84cc82255312011a059490c4506185397
tracer-ics2022 <tracer@njuics.org>
2023-06-01 15:44:04 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:44:04 up 37 min,  1 user,  load average: 1.69, 0.99, 0.75

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -68,9 +68,9 @@ static void exec_once(Decode *s, vaddr_t pc) {
   char *p = s->logbuf;
   p += snprintf(p, sizeof(s->logbuf), FMT_WORD ":", s->pc);
   int ilen = s->snpc - s->pc;
-  int i, mlen;
+  int i;
   uint8_t *inst = (uint8_t *)&s->isa.inst.val;
-  for (i = ilen - 1, mlen = 20; i >= 0 && mlen >= 0; i --, mlen --) {
+  for (i = ilen - 1; i >= 0 ; i --) {
     p += snprintf(p, 4, " %02x", inst[i]);
   }
   int ilen_max = MUXDEF(CONFIG_ISA_x86, 8, 4);
