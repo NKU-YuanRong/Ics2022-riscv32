9dacf7af671aeedda21b0ca8225e943a746045ae
tracer-ics2022 <tracer@njuics.org>
2023-06-08 13:13:22 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:13:22 up  6:01,  1 user,  load average: 0.38, 0.36, 0.31

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -19,6 +19,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * Then return the address of the interrupt/exception vector.
    */
+  
   return 0;
 }
 
