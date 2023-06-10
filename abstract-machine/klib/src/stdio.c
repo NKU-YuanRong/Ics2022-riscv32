08e1f23a49c54d133ddd259505e28a2c3bf1830b
tracer-ics2022 <tracer@njuics.org>
2023-06-10 18:21:11 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:21:11 up  2:45,  1 user,  load average: 0.81, 0.73, 0.60

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,46 +5,111 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
-int vsprintf(char *out, const char *fmt, va_list ap);
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap);
+// static char* get_int(char *p, va_list *ap) {
+// 	int d = va_arg(*ap, int);
+// 	char str[32];
+// 	int len = 0;
+// 	if (d == 0) {
+// 		*p++ = '0';
+// 		return p;
+// 	}
+// 	if (d < 0) {
+// 		*p++ = '-';
+// 		d *= -1;
+// 	}
+// 	while (d) {
+// 		str[len++] = d % 10 + '0';
+// 		d /= 10;
+// 	}
+// 	for (int i = len - 1; i >= 0; i--) {
+// 		*p++ = str[i];
+// 	}
+// 	return p;
+// }
 
-int printf(const char *fmt, ...) {
-	char buffer[2048];
-	va_list arg;
-	va_start (arg, fmt);
-	int done = vsprintf(buffer, fmt, arg);
-	putstr(buffer);
-	va_end(arg);
-	return done;
-}
+// static char* get_string(char* p, va_list *ap) {
+// 	char *str = va_arg(*ap, char*);
+// 	while(*str) {
+// 		*p++ = *str++;
+// 	}
+// 	return p;
+// }
+
+// static char* get_char(char* p, va_list *ap) {
+// 	char ch = (char)va_arg(*ap, int);
+// 	*p++ = ch;
+// 	return p;
+// }
+
+// static char* get_hex(char* p, va_list *ap) {
+// 	unsigned int value = va_arg(*ap, unsigned int);
+// 	if(value == 0){
+// 		*p++ = '0';
+// 		return p;
+// 	}
+// 	for (; value != 0; value /= 16) {
+// 		if (value % 16 < 10) {
+// 			*p++ = value % 16 + '0';
+// 		}
+// 		else {
+// 			*p++ = value % 16 - 10 + 'A';
+// 		}
+// 	}
+// 	return p;
+// }
+
+// static char* get_point(char* p, va_list *ap) {
+// 	uint32_t pointer = va_arg(*ap, uint32_t);
+// 	for (; pointer != 0; pointer /= 16) {
+// 		if (pointer % 16 < 10) {
+// 			*p++ = pointer % 16 + '0';
+// 		}
+// 		else {
+// 			*p++ = pointer % 16 - 10 + 'A';
+// 		}
+// 	}
+// 	return p;
+// }
 
 static char HEX_CHARACTERS[] = "0123456789ABCDEF";
 #define BIT_WIDE_HEX 8
-
-int vsprintf(char *out, const char *fmt, va_list ap) {
-  	return vsnprintf(out, -1, fmt, ap);
-}
-
-int sprintf(char *out, const char *fmt, ...) {
-	va_list valist;
-	va_start(valist, fmt);
-	int res = vsprintf(out ,fmt, valist);
-	va_end(valist);
-	return res;
-}
-
-int snprintf(char *out, size_t n, const char *fmt, ...) {
-	va_list arg;
-	va_start (arg, fmt);
-	int done = vsnprintf(out, n, fmt, arg);
-	va_end(arg);
-	return done;
-}
-
 #define append(x) {out[j++]=x; if (j >= n) {break;}}
 
-int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
-	char buffer[128];
+int get_result(char *out, size_t n, const char *fmt, va_list ap) {
+	// char* p = (char*)out;
+	// while (*fmt) {
+	// 	if (*fmt == '%') {
+	// 		if ((uint32_t)p - (uint32_t)out >= n)
+	// 			break;
+	// 		fmt++;
+	// 		switch (*fmt) {
+	// 			case 'd': 
+	// 				p = get_int(p, &ap);
+	// 				break;
+	// 			case 's': 
+	// 				p = get_string(p, &ap);
+	// 				break;
+	// 			case 'c':
+	// 				p = get_char(p, &ap);
+	// 				break;
+	// 			case 'p':
+	// 				p = get_point(p, &ap);
+	// 				break;
+	// 			case 'x':
+	// 				p = get_hex(p, &ap);
+	// 				break;
+	// 		}
+	// 		fmt++;
+	// 	}
+	// 	else {
+	// 		if ((uint32_t)p - (uint32_t)out >= n)
+	// 			break;
+	// 		*p++ = *fmt++;
+	// 	}
+	// }
+	// *p++ = '\0';
+	// return (uint32_t)p - (uint32_t)out - 1;
+		char buffer[128];
 	char *txt, cha;
 	int num, len;
 	unsigned int unum;
@@ -124,6 +189,42 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
 	}
 	out[j] = '\0';
 	return j;
+}
+
+int printf(const char *fmt, ...) {
+	char out[1024];
+	va_list ap;
+	va_start(ap, fmt);
+	int len = get_result(out, -1, fmt, ap);
+	va_end(ap);
+	putstr(out);
+	return len;
+}
+
+int vsprintf(char *out, const char *fmt, va_list ap) {
+  	int len = get_result(out, -1, fmt, ap);
+	return len;
+}
+
+int sprintf(char *out, const char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+	int len = get_result(out, -1, fmt, ap);
+	va_end(ap);
+	return len;
+}
+
+int snprintf(char *out, size_t n, const char *fmt, ...) {
+	va_list ap;
+	va_start(ap, fmt);
+	int len = get_result(out, n, fmt, ap);
+	va_end(ap);
+	return len;
+}
+
+int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
+  	int len = get_result(out, n, fmt, ap);
+	return len;
 }
 
 #endif
