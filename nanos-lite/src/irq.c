8f9d6cbac848fbea488bda81554bc55b0bc93c00
tracer-ics2022 <tracer@njuics.org>
2023-06-11 14:12:40 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:12:40 up 10:43,  1 user,  load average: 0.38, 0.65, 0.62

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -2,6 +2,7 @@
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
+  	case 1: Log("Yield handle successfully!");
     default: panic("Unhandled event ID = %d", e.event);
   }
 
