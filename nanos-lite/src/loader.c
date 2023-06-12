eab93028e5d12f78e7ab49bcc73d0280d1546c1a
tracer-ics2022 <tracer@njuics.org>
2023-06-12 21:46:51 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:46:51 up 16:51,  1 user,  load average: 0.95, 0.82, 0.79

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -13,45 +13,39 @@ extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
 extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
 extern size_t get_ramdisk_size();
 static uintptr_t loader(PCB *pcb, const char *filename) {
-  Elf_Ehdr ehdr;
-  ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
-  assert((*(uint32_t *)ehdr.e_ident == 0x464c457f));
+  // Elf_Ehdr ehdr;
+  // ramdisk_read(&ehdr, 0, sizeof(Elf_Ehdr));
+  // assert((*(uint32_t *)ehdr.e_ident == 0x464c457f));
 
-  Elf_Phdr phdr[ehdr.e_phnum];
-  ramdisk_read(phdr, ehdr.e_phoff, sizeof(Elf_Phdr)*ehdr.e_phnum);
-  for (int i = 0; i < ehdr.e_phnum; i++) {
-    if (phdr[i].p_type == PT_LOAD) {
-      ramdisk_read((void*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
-      memset((void*)(phdr[i].p_vaddr+phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
+  // Elf_Phdr phdr[ehdr.e_phnum];
+  // ramdisk_read(phdr, ehdr.e_phoff, sizeof(Elf_Phdr)*ehdr.e_phnum);
+  // for (int i = 0; i < ehdr.e_phnum; i++) {
+  //   if (phdr[i].p_type == PT_LOAD) {
+  //     ramdisk_read((void*)phdr[i].p_vaddr, phdr[i].p_offset, phdr[i].p_memsz);
+  //     memset((void*)(phdr[i].p_vaddr+phdr[i].p_filesz), 0, phdr[i].p_memsz - phdr[i].p_filesz);
+  //   }
+  // }
+  // return ehdr.e_entry;
+
+  Elf_Ehdr elf;
+  int fd=fs_open(filename);
+  fs_lseek(fd, 0, SEEK_SET);
+  fs_read(fd, &elf, sizeof(Elf_Ehdr));
+  Elf_Phdr Phdr;
+  assert(*(uint32_t*)elf.e_ident==0x464c457f);
+  //printf("success!\n");
+  for (int i = 0; i < elf.e_phnum; i++) {
+    fs_lseek(fd, elf.e_phoff + i*elf.e_phentsize, SEEK_SET);//TODO：改为CUR
+    fs_read(fd, &Phdr, sizeof(Phdr));
+    if (Phdr.p_type == 1) {
+      fs_lseek(fd, Phdr.p_offset, SEEK_SET);
+      fs_read(fd, (void*)Phdr.p_vaddr, Phdr.p_filesz);
+      for(unsigned int i = Phdr.p_filesz; i < Phdr.p_memsz;i++){
+          ((char*)Phdr.p_vaddr)[i] = 0;
+      }
     }
   }
-  return ehdr.e_entry;
-
-  // Elf32_Ehdr header;
-	// int fd = fs_open(filename, 0, 0);
-  // /*
-  // * Check whether the elf-file meets the specification 
-  // */
-	// fs_read(fd, &header, sizeof(Elf32_Ehdr));
-	// if (*(uint32_t *)header.e_ident == (uint32_t)0X464C457F){
-	// 	Log("header.e_ident=%x,it should be 0x464c457f",*(uint32_t *)header.e_ident );
-  //   //assert(0);
-	// }
-  
-	// Elf32_Phdr pro_header[8];
-
-	// fs_lseek(fd, header.e_phoff, SEEK_SET);
-
-	// fs_read(fd, pro_header, sizeof(Elf32_Phdr) * header.e_phnum);
-	// for (int i = 0; i < header.e_phnum; i++){
-	// 	if (pro_header[i].p_type == PT_LOAD){
-	// 		fs_lseek(fd, pro_header[i].p_offset, SEEK_SET);
-	// 		fs_read(fd, (void *)(pro_header[i].p_vaddr), pro_header[i].p_filesz);
-	// 		memset((void *)(pro_header[i].p_vaddr + pro_header[i].p_filesz), 0, pro_header[i].p_memsz - pro_header[i].p_filesz);
-	// 	}
-	// }
-	// fs_close(fd);
-	// return header.e_entry;
+  return elf.e_entry;
 }
 
 void naive_uload(PCB *pcb, const char *filename) {
