3efc4d9c001e5eeaeb1cb9969cd462c4b39ae32d
tracer-ics2022 <tracer@njuics.org>
2023-06-12 22:35:46 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:35:46 up 17:40,  1 user,  load average: 0.72, 0.64, 0.74

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -28,8 +28,8 @@ void init_proc() {
 
   // load program here
   // naive_uload(NULL, "/bin/dummy");
-  naive_uload(NULL, "/bin/hello");
-  // naive_uload(NULL, "/bin/file-test");
+  // naive_uload(NULL, "/bin/hello");
+  naive_uload(NULL, "/bin/file-test");
 }
 
 Context* schedule(Context *prev) {
