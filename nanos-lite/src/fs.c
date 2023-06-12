672b83f2d1fc86e26e6ff4755406c03250812607
tracer-ics2022 <tracer@njuics.org>
2023-06-12 23:04:33 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:04:33 up 18:09,  1 user,  load average: 1.13, 0.83, 0.75

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -62,8 +62,6 @@ int fs_open(const char *path){
   	for (int i = FD_FB + 1; i < FD_SIZE; i++) {
     if (strcmp(path, file_table[i].name) == 0) {
       file_table[i].open_offset = 0;
-      // file_table[i].read=*ramdisk_read;
-      // file_table[i].write=*ramdisk_write;
       return i;
     }
   }
@@ -80,24 +78,35 @@ size_t fs_read(int fd,void *buf,size_t len){
   // file_table[fd].open_offset += count;
   // return count;
 
-  Finfo *info = &file_table[fd];
-  size_t real_len;
-  if (info->read){
-    real_len = info->read(buf, info->open_offset, len);
+  // Finfo *info = &file_table[fd];
+  // size_t real_len;
+  // if (info->read){
+  //   real_len = info->read(buf, info->open_offset, len);
+  // }
+  // else {
+  //   real_len = info->open_offset + len <= info->size ?
+  //   len : info->size - info->open_offset;
+  //   ramdisk_read(buf, info->disk_offset + info->open_offset, real_len);
+  // }
+  // info->open_offset += real_len;
+  // return real_len;
+
+  int actual_len = 0;
+  if (file_table[fd].open_offset + len >= file_table[fd].size) {
+    actual_len = file_table[fd].size > 0 ? file_table[fd].size : 0;
   }
   else {
-    real_len = info->open_offset + len <= info->size ?
-    len : info->size - info->open_offset;
-    ramdisk_read(buf, info->disk_offset + info->open_offset, real_len);
+    actual_len = len;
   }
-  info->open_offset += real_len;
-  return real_len;
 
-  // int actual_len = 0;
-  // if (file_table[fd].open_offset + len >= file_table[fd].size) {
-  //   actual_len = file_table[fd].size - file_table[fd].open_offset;
-  // }
-
+  if (file_table[fd].read) {
+    file_table[fd].read(buf, file_table[fd].open_offset, actual_len);
+  }
+  else {
+    ramdisk_read(buf, file_table[fd].open_offset, actual_len);
+  }
+  file_table[fd].open_offset += actual_len;
+  return actual_len;
 }
 
 size_t fs_lseek(int fd, size_t offset, int whence){
