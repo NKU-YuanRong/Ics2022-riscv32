d5a058b7f67faae8b4ad5bf13865692c1c2aba27
tracer-ics2022 <tracer@njuics.org>
2023-06-13 17:19:18 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:19:18 up 20:48,  1 user,  load average: 0.09, 0.30, 0.33

--- navy-apps/tests/event-test/main.c
+++ navy-apps/tests/event-test/main.c
@@ -5,7 +5,7 @@ int main() {
   NDL_Init(0);
   while (1) {
     char buf[64];
-    if (NDL_PollEvent(buf, sizeof(buf))) {
+    if (NDL_PollEvent(buf, sizeof(buf)) != 0) {
       printf("receive event: %s\n", buf);
     }
   }
