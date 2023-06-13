55ea5378a54d7ad378e780be681c6f9725f6c1e7
tracer-ics2022 <tracer@njuics.org>
2023-06-13 22:51:29 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:51:29 up 6 min,  1 user,  load average: 0.55, 0.96, 0.58

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -34,8 +34,8 @@ void init_proc() {
   // naive_uload(NULL, "/bin/event-test");
   // naive_uload(NULL, "/bin/bmp-test");
   // naive_uload(NULL, "/bin/menu");
-  naive_uload(NULL, "/bin/nterm");
-  // naive_uload(NULL, "/bin/bird");
+  // naive_uload(NULL, "/bin/nterm");
+  naive_uload(NULL, "/bin/bird");
 }
 
 Context* schedule(Context *prev) {
