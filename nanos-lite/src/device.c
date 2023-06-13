7f89c991515da588e9fab05eb842a85d488797a4
tracer-ics2022 <tracer@njuics.org>
2023-06-13 17:16:59 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:16:59 up 20:46,  1 user,  load average: 0.44, 0.41, 0.37

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -22,7 +22,7 @@ size_t serial_write(const void *buf, size_t offset, size_t len) {
 }
 
 size_t events_read(void *buf, size_t offset, size_t len) {
-  int actual_len = 0;
+  size_t actual_len = 0;
   AM_INPUT_KEYBRD_T ev = io_read(AM_INPUT_KEYBRD);
   if (ev.keycode == AM_KEY_NONE) {
     actual_len = 0;
