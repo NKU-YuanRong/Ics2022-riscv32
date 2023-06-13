18ac998733c796b2b7cc448e8e97d13402818a9b
tracer-ics2022 <tracer@njuics.org>
2023-06-13 16:11:52 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:11:52 up 19:41,  1 user,  load average: 0.35, 0.36, 0.56

--- navy-apps/tests/timer-test/timer-test.c
+++ navy-apps/tests/timer-test/timer-test.c
@@ -5,7 +5,6 @@
 #include <NDL.h>
 
 int main(){
-  struct timeval dut;struct timezone tz;
   /*
   NDL_Init(0);
 
@@ -21,26 +20,30 @@ int main(){
   }
 
   NDL_Quit();*/
+  
+  struct timeval dut;
+  struct timezone tz;
+  
   dut.tv_sec = 0;
   dut.tv_usec = 0;
   
   struct timeval ref;
   
-  gettimeofday(&ref,&tz);
+  gettimeofday(&ref, &tz);
   ref.tv_usec = 500000;
   
   while(1){
-    gettimeofday(&dut,&tz);
-    if(dut.tv_sec==ref.tv_sec){
-      if(ref.tv_usec==0&&ref.tv_usec<dut.tv_usec){
-        // printf("sec:%ld  usec:%ld\n",ref.tv_sec,ref.tv_usec / 1000);
+    gettimeofday(&dut, &tz);
+    
+    if(dut.tv_sec == ref.tv_sec){
+      if(ref.tv_usec == 0 && ref.tv_usec < dut.tv_usec){
         printf("now time: %ld.%lds\n", ref.tv_sec, ref.tv_usec / 1000);
-        ref.tv_usec=500000;
+        ref.tv_usec = 500 * 1000;
       }
-      else if(ref.tv_usec==500000&&ref.tv_usec<dut.tv_usec){
-        printf("sec:%ld  usec:%ld\n",ref.tv_sec,ref.tv_usec);
-        ref.tv_usec=0;
-        ref.tv_sec++;
+      else if(ref.tv_usec == 500000 && ref.tv_usec < dut.tv_usec){
+        printf("now time: %ld.%lds\n", ref.tv_sec, ref.tv_usec / 1000);
+        ref.tv_usec = 0;
+        ref.tv_sec += 1;
       }
     }
   }
