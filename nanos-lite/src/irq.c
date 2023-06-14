45957bf4538abfade19e9e773ea07cc8cd720bd8
tracer-ics2022 <tracer@njuics.org>
2023-06-14 15:21:05 +0800

    >  compile NEMU 2012543 YANCHEN LIU Linux yyy-virtual-machine 5.19.0-35-generic #36~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb 17 15:17:25 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:21:05 up  1:46,  1 user,  load average: 0.56, 0.48, 0.40

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,11 +1,12 @@
 #include <common.h>
+#include <proc.h>
 
 extern void do_syscall(Context*);
 
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
-  	// case 1: Log("Yield handle successfully!"); c->mepc += 4; break;
-    case 1: do_syscall(c); c->mepc += 4; break;
+  	case 1: // Log("Yield handle successfully!"); c->mepc += 4; break;
+      Log("new yield!"); c->GPRx = (unsigned int)schedule(c); c->mepc += 4; break;
     case 2: do_syscall(c); c->mepc += 4; break;
     default: panic("Unhandled event ID = %d", e.event);
   }
